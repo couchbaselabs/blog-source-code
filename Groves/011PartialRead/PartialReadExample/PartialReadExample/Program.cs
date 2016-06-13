164afd433a3cb7e78d370c85f5cc5e318a897c6a
Matthew Groves <me@mgroves.com>
2016-06-13 10:38:36 -0400

    updating partial read blog post and code

--- Groves/011PartialRead/PartialReadExample/PartialReadExample/Program.cs
+++ Groves/011PartialRead/PartialReadExample/PartialReadExample/Program.cs
@@ -17,62 +17,62 @@ namespace PartialReadExample
         {
             SetupCouchbase();
 
-            Bug();
+//            Bug();
 
-//            var key = "Profile::" + _random.Next(10000);
-//            InsertADocument(key);
-//
-//            // tag::WholeDocument[]
-//            var wholeDocument = _bucket.Get<dynamic>(key).Value;
-//            Console.WriteLine($"===Whole document (Key = {key})");
-//            Console.WriteLine(JsonConvert.SerializeObject(wholeDocument,Formatting.Indented));
-//            // end::WholeDocument[]
-//            Console.WriteLine();
-//
-//            // tag::TryShowSubDocument[]
-//            ShowSubdocument(key, "username");
-//            ShowSubdocument(key, "profile");
-//            ShowSubdocument(key, "profile.phoneNumber");
-//            ShowSubdocument(key, "profile.address.street");
-//            ShowSubdocument(key, "profile.address.province");
-//            // end::TryShowSubDocument[]
-//
-//            // tag::TryDoesSubdocumentExist[]
-//            DoesSubdocumentExist(key, "profile.address.state");
-//            DoesSubdocumentExist(key, "profile.address.province");
-//            // end::TryDoesSubdocumentExist[]
+            var key = "Profile::" + _random.Next(10000);
+            InsertADocument(key);
+
+            // tag::WholeDocument[]
+            var wholeDocument = _bucket.Get<dynamic>(key).Value;
+            Console.WriteLine($"===Whole document (Key = {key})");
+            Console.WriteLine(JsonConvert.SerializeObject(wholeDocument,Formatting.Indented));
+            // end::WholeDocument[]
+            Console.WriteLine();
+
+            // tag::TryShowSubDocument[]
+            ShowSubdocument(key, "username");
+            ShowSubdocument(key, "profile");
+            ShowSubdocument(key, "profile.phoneNumber");
+            ShowSubdocument(key, "profile.address.street");
+            ShowSubdocument(key, "profile.address.province");
+            // end::TryShowSubDocument[]
+
+            // tag::TryDoesSubdocumentExist[]
+            DoesSubdocumentExist(key, "profile.address.state");
+            DoesSubdocumentExist(key, "profile.address.province");
+            // end::TryDoesSubdocumentExist[]
 
             CleanupCouchbase();
         }
 
-        private static void Bug()
-        {
-            var doc = new Document<dynamic>
-            {
-                Id = "Foo::123",
-                Content = new
-                {
-                    Username = "mgroves",
-                    Profile = new
-                    {
-                        PhoneNumber = "123-456-7890",
-                        Address = new
-                        {
-                            Street = "123 Main Rd",
-                            City = "Columbus",
-                            State = "Ohio"
-                        }
-                    }
-                }
-            };
-            _bucket.Upsert(doc);
-
-            var subDoc = _bucket.LookupIn<dynamic>("Foo::123").Exists("profile.address.state").Execute();
-            Console.WriteLine("Subdoc profile.address.state exists: " + subDoc.Exists("profile.address.state"));
-
-            var subDoc2 = _bucket.LookupIn<dynamic>("Foo::123").Exists("profile.address.province").Execute();
-            Console.WriteLine("Subdoc profile.address.province exists: " + subDoc2.Exists("profile.address.province"));
-        }
+//        private static void Bug()
+//        {
+//            var doc = new Document<dynamic>
+//            {
+//                Id = "Foo::123",
+//                Content = new
+//                {
+//                    Username = "mgroves",
+//                    Profile = new
+//                    {
+//                        PhoneNumber = "123-456-7890",
+//                        Address = new
+//                        {
+//                            Street = "123 Main Rd",
+//                            City = "Columbus",
+//                            State = "Ohio"
+//                        }
+//                    }
+//                }
+//            };
+//            _bucket.Upsert(doc);
+//
+//            var subDoc = _bucket.LookupIn<dynamic>("Foo::123").Exists("profile.address.state").Execute();
+//            Console.WriteLine("Subdoc profile.address.state exists: " + subDoc.Exists("profile.address.state"));
+//
+//            var subDoc2 = _bucket.LookupIn<dynamic>("Foo::123").Exists("profile.address.province").Execute();
+//            Console.WriteLine("Subdoc profile.address.province exists: " + subDoc2.Exists("profile.address.province"));
+//        }
 
         // tag::DoesSubdocumentExist[]
         private static void DoesSubdocumentExist(string key, string path)
