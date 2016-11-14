1d62477485c2caa280f04415ca454b55d2f1a614
mgroves <me@mgroves.com>
2016-11-14 11:52:26 -0500

    initial commit of screenshots and code for the blog post

---
+++ Groves/036DockerNetCore/DockerNetCore/src/DockerNetCore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using DockerNetCore.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var gifts = Gift.GetAllGifts();
+            return View(gifts);
+        }
+
+        public IActionResult AddGift()
+        {
+            Gift.CreateNewGift();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Groves/036DockerNetCore/DockerNetCore/src/DockerNetCore/Model/Gift.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Couchbase;
+using Couchbase.N1QL;
+
+namespace DockerNetCore.Model
+{
+    public class Gift
+    {
+        public string GiftName { get; set; }
+        public decimal Price { get; set; }
+
+        public static List<Gift> GetAllGifts()
+        {
+            var bucket = ClusterHelper.GetBucket("default");
+            var query = QueryRequest.Create("SELECT g.* FROM `default` g");
+            query.ScanConsistency(ScanConsistency.RequestPlus);
+            return bucket.Query<Gift>(query).Rows;
+        }
+
+        public static void CreateNewGift()
+        {
+            var gift = new Gift();
+            gift.GiftName = GetRandomGiftName();
+            gift.Price = new Random().Next(1000, 5000)/100.0M;
+            var bucket = ClusterHelper.GetBucket("default");
+            bucket.Insert(new Document<Gift> {Id = Guid.NewGuid().ToString(), Content = gift});
+        }
+
+        private static string GetRandomGiftName()
+        {
+            var rand = new Random();
+            var giftNames = new List<string>
+            {
+                "Candy", "Nuts", "Rocking horse", "Doll", "Mittens", "Gloves", "Toy train", "Books", "Handkerchief", "Skates",
+                "Furby", "Robopuppy", "iPad", "Lego", "Nerf", "BB-8", "Shopkins"
+            };
+            return giftNames[rand.Next(0, giftNames.Count - 1)] + " #" + rand.Next(100,999);
+        }
+    }
+}
