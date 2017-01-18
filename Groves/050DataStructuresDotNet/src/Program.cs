766a91731f01b5cb29294b67720aa2b8c4bc1b7a
mgroves <me@mgroves.com>
2017-01-18 10:22:33 -0500

    *** WHOOPS *** I misnumbered blog post folders (and image files) so this commit corrects them

---
+++ Groves/050DataStructuresDotNet/src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Couchbase;
+using Couchbase.Collections;
+using Couchbase.Configuration.Client;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClusterHelper.Initialize(new ClientConfiguration {
+                Servers = new List<Uri> { new Uri("couchbase://localhost")}
+            });
+            var bucket = ClusterHelper.GetBucket("data-structures");
+
+            var list = new CouchbaseList<dynamic>(bucket, "myList");
+            list.Add(new { foo = "bar" + Guid.NewGuid().ToString()});
+
+            var map = new CouchbaseDictionary<string,string>(bucket, "myMap");
+            map.Add("key" + Guid.NewGuid().ToString(), "value");
+
+            ClusterHelper.Close();
+        }
+    }
+}
