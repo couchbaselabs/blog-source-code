85075121547edeabd58a52caf80f54d018d4771c
mgroves <me@mgroves.com>
2017-02-13 15:55:55 -0500

    46 and 47 - parts 2 and 3 of sql server series, moved sproc and friends to 47, reorganized 47

--- Groves/045MigrateFromSQLServer1/src/SQLServerToCouchbase/SQLServerDataAccess/Scripts/05Sproc.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE SP_SEARCH_SHOPPING_CART_BY_NAME
-	@searchString NVARCHAR(50)
-AS
-BEGIN
-	SELECT c.Id, c.[User], c.DateCreated
-	FROM ShoppingCart c
-	WHERE c.[User] LIKE '%' + @searchString + '%'
-END
-GO
--- Groves/045MigrateFromSQLServer1/src/SQLServerToCouchbase/SQLServerToCouchbase.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SQLServerToCouchbase.Web.MvcApplication" Language="C#" %>
