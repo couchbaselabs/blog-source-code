3cf301d6e488c405498e230b5a03832e02e99693
mgroves <me@mgroves.com>
2017-06-02 10:41:17 -0400

    68 - enable logging blog post / video

---
+++ Groves/068EnableLoggingDotNetVideo/src/loggingsample/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Couchbase;
+using Couchbase.Authentication;
+using Couchbase.Configuration.Client;
+
+namespace loggingsample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            log4net.GlobalContext.Properties["LogFileLocation"] = Path.Combine(Server.MapPath("~/Logs"), "LogFile068.txt");
+            log4net.Config.XmlConfigurator.Configure();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ClusterHelper.Initialize(new ClientConfiguration
+            {
+                Servers = new List<Uri> {  new Uri("http://localhost:8091")}
+            });
+            var cluster = ClusterHelper.Get();
+            var creds = new PasswordAuthenticator("myuser", "password");
+            cluster.Authenticate(creds);
+        }
+    }
+}
---
+++ Groves/068EnableLoggingDotNetVideo/src/loggingsample/Models/FooBar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace loggingsample.Models
+{
+    public class FooBar
+    {
+        public string Foo { get; set; }
+        public string Bar { get; set; }
+
+        public static FooBar Generate()
+        {
+            return new FooBar
+            {
+                Foo = Path.GetRandomFileName(),
+                Bar = new Random().Next(1, 1000000).ToString()
+            };
+        }
+    }
+}
