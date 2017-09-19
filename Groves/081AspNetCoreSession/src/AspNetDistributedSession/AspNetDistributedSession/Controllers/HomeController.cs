84ec2250031325a88214d67fb565f2d84f83953f
mgroves <me@mgroves.com>
2017-09-19 11:03:03 -0400

    81 - first draft of couchbase extensions session blog post

---
+++ Groves/081AspNetCoreSession/src/AspNetDistributedSession/AspNetDistributedSession/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Couchbase.Extensions.Session;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetDistributedSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // tag::Another[]
+            HttpContext.Session.SetObject("sessionkey2", new
+            {
+                Address = "123 Main St",
+                City = "Lancaster",
+                State = "OH"
+            });
+            // tag::Another[]
+
+            return View();
+        }
+
+        // tag::About[]
+        public IActionResult About()
+        {
+            HttpContext.Session.SetObject("sessionkey", new
+            {
+                Name = "Matt",
+                Twitter = "@mgroves",
+                Guid = DateTime.Now
+            });
+
+            ViewData["Message"] = "I put a value in your session. Click 'Contact' to see it.";
+
+            return View();
+        }
+        // end::About[]
+
+        // tag::Contact[]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = HttpContext.Session.GetObject<dynamic>("sessionkey");
+
+            return View();
+        }
+        // end::Contact[]
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Groves/081AspNetCoreSession/src/AspNetDistributedSession/AspNetDistributedSession/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+// tag::namespaces[]
+using Couchbase.Extensions.Caching;
+using Couchbase.Extensions.DependencyInjection;
+using Couchbase.Extensions.Session;
+// end::namespaces[]
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetDistributedSession
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            /* this will setup the standard in-process session
+           // tag::ConfigureSession[]
+           services.AddDistributedMemoryCache();
+           services.AddSession();
+           // end::ConfigureSession[]
+           */
+
+            // tag::CouchbaseConfig[]
+            services.AddCouchbase(opt =>
+            {
+                opt.Servers = new List<Uri> { new Uri("http://localhost:8091") };
+            });
+
+            services.AddDistributedCouchbaseCache("sessionstore", "password", opt => { });
+            // end::CouchbaseConfig[]
+
+            // tag::CouchbaseSession[]
+            services.AddCouchbaseSession(opt =>
+            {
+                opt.CookieName = ".MyApp.Cookie";
+                opt.IdleTimeout = new TimeSpan(0, 0, 20, 0);
+            });
+            // end::CouchbaseSession[]
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            // this will tell ASP.NET Core to use session
+            // tag::UseSession[]
+            app.UseSession();
+            // end::UseSession[]
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            // tag::cleanup[]
+            appLifetime.ApplicationStopped.Register(() =>
+            {
+                app.ApplicationServices.GetRequiredService<ICouchbaseLifetimeService>().Close();
+            });
+            // end::cleanup[]
+        }
+    }
+}
