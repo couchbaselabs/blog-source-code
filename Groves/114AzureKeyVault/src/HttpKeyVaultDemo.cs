085f6c783b0215b755b8aa95f4d3f938b2181731
Matthew D. Groves <me@mgroves.com>
2018-10-08 15:07:16 -0400

    114 - first draft of blog post with images

--- Groves/114AzureKeyVault/src/HttpKeyVaultDemo.cs
+++ Groves/114AzureKeyVault/src/HttpKeyVaultDemo.cs
@@ -25,11 +25,14 @@ namespace CbKeyVault.Function
     {
         private static HttpClient client = new HttpClient();
 
+        // tag::uris[]
         const string Vault_Bucketname_Uri = "https://mycouchbasekeyvault.vault.azure.net/secrets/cbBucketName/1bda709e1372465a8f03b6e8c3fb6014";
         const string Vault_Clusteruri_Uri = "https://mycouchbasekeyvault.vault.azure.net/secrets/cbClusterUri/48605e696b3645a6a7c396a15d636dc2";
         const string Vault_Username_Uri = "https://mycouchbasekeyvault.vault.azure.net/secrets/cbUsername/7d73ef5fa2174e5491d4a50a42bb0800";
         const string Vault_Password_Uri = "https://mycouchbasekeyvault.vault.azure.net/secrets/cbPassword/d6f61ff7e41a4fdcbe17de0b1fe1f115";
+        // end::uris[]
 
+        // tag::lazy[]
         private static readonly Lazy<IBucket> Bucket = new Lazy<IBucket>(() =>
         {
             var uri = GetSecret(Vault_Clusteruri_Uri);
@@ -43,14 +46,19 @@ namespace CbKeyVault.Function
             cluster.Authenticate(username, password);
             return cluster.OpenBucket(bucketName);
         });
+        // end::lazy[]
 
+        // tag::GetSecret[]
         private static string GetSecret(string url)
         {
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback), client);
-            var secret = kvClient.GetSecretAsync(url).Result.Value;  // using async method synchronously, would need to use AsyncLazy above otherwise
+            var kvClient = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(
+                    azureServiceTokenProvider.KeyVaultTokenCallback), client);
+            var secret = kvClient.GetSecretAsync(url).Result.Value;
             return secret;
         }
+        // end::GetSecret[]
 
         [FunctionName("HttpTriggerCSharpGet")]
         public static async Task<IActionResult> Get([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
