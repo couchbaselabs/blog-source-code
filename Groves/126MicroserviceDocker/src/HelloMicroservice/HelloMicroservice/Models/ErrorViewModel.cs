9510518d318bd893952c57f5d9a0d9b1567e0467
Matthew D. Groves <me@mgroves.com>
2019-10-28 14:59:58 -0400

    126 - first draft of asp.net core microservices blog post

---
+++ Groves/126MicroserviceDocker/src/HelloMicroservice/HelloMicroservice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloMicroservice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
