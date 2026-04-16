145999ac9d8549d3a24884f7990484fca43126d1
Matt Groves <me@mgroves.com>
2026-04-16 14:34:12 -0400

    updated blog post and source code

--- Groves/072LoadingXmlToCouchbase/LoadingXml/LoadingXml/Program.cs
+++ Groves/072LoadingXmlToCouchbase/LoadingXml/LoadingXml/Program.cs
@@ -1,30 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Xml;
 using Couchbase;
-using Couchbase.Configuration.Client;
+using Couchbase.KeyValue;
 using Newtonsoft.Json;
 using Formatting = Newtonsoft.Json.Formatting;
 
 namespace LoadingXml
 {
-    // http://www.newtonsoft.com/json/help/html/ConvertingJSONandXML.htm
-    // http://www.newtonsoft.com/json/help/html/Introduction.htm
-
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // sample XML, parsed into an XmlDocument
             // this might come from an XML file, another database, a REST API, etc
             // but for this example, it's just a hardcoded string
             // tag::xml[]
             var xml = @"
-<Invoice>
-    <Timestamp>1/1/2017 00:01</Timestamp>
-    <CustNumber>12345</CustNumber>
-    <AcctNumber>54321</AcctNumber>
-</Invoice>";
+                <Invoice>
+                    <Timestamp>4/16/2026 02:23</Timestamp>
+                    <CustNumber>12345</CustNumber>
+                    <AcctNumber>54321</AcctNumber>
+                </Invoice>";
             // end::xml[]
             // tag::xmldocument[]
             XmlDocument doc = new XmlDocument();
@@ -37,26 +33,30 @@ namespace LoadingXml
             // end::jsonconvert[]
 
             // this is just an example of what the Json would look like if I DIDN'T omit root node
-            // {"Invoice":{"Timestamp":"1/1/2017 00:01","CustNumber":"12345","AcctNumber":"54321"}}
+            // {"Invoice":{"Timestamp":"4/16/2026 02:23","CustNumber":"12345","AcctNumber":"54321"}}
 
             // connect to couchbase cluster
-            ClusterHelper.Initialize(new ClientConfiguration
-            {
-                Servers = new List<Uri> {new Uri("http://localhost:8091")}
-            });
-            var bucket = ClusterHelper.GetBucket("loadxml", "password");
+            var cluster = await Cluster.ConnectAsync(
+                "couchbases://cb.ojzftkgudoak8tkc.cloud.couchbase.com",
+                "xmlconvert",
+                "5M1+cjb$LAhP"
+            );
+            var bucket = await cluster.BucketAsync("loadxml");
+            var collection = bucket.DefaultCollection();
 
             // insert directly (literal translation)
             // tag::insertobject[]
             object transactObject1 = JsonConvert.DeserializeObject(json);
-            bucket.Insert(Guid.NewGuid().ToString(), transactObject1);
+            await collection.InsertAsync(Guid.NewGuid().ToString(), transactObject1);
             // end::insertobject[]
 
             // insert via class (type information, naming conventions applied)
             // tag::insertobject2[]
             Invoice transactObject2 = JsonConvert.DeserializeObject<Invoice>(json);
-            bucket.Insert(Guid.NewGuid().ToString(), transactObject2);
+            await collection.InsertAsync(Guid.NewGuid().ToString(), transactObject2);
             // end::insertobject2[]
+
+            await cluster.DisposeAsync();
         }
     }
 
